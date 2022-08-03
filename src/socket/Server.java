e0e7a3e530c7d268b3f6b4e63cdbe254aa6dda51
Freg <Freg@personal.com>
2022-08-03 11:53:22 +0800

    线程1

--- src/socket/Server.java
+++ src/socket/Server.java
@@ -22,19 +22,20 @@ public class Server {
     }
     public void start(){
         try {
-            System.out.println("等待客户端连接");
             //accept()为一个阻塞方法
-            Socket socket = serverSocket.accept();
-            System.out.println("一个客户端连接了");
+            while(true){
+                System.out.println("等待客户端连接");
+                Socket socket = serverSocket.accept();
+                System.out.println("一个客户端连接了");
+                String line;
+                //通过刚接受链接的socket获取输入流来读取该客户端发送来的消息
+                InputStream is = socket.getInputStream();
+                BufferedReader br = new BufferedReader
+                        (new InputStreamReader(is, StandardCharsets.UTF_8));
 
-            String line;
-            //通过刚接受链接的socket获取输入流来读取该客户端发送来的消息
-            InputStream is = socket.getInputStream();
-            BufferedReader br = new BufferedReader
-                    (new InputStreamReader(is, StandardCharsets.UTF_8));
-
-            while((line=br.readLine())!=null){
-                System.out.println("客户端说："+line);
+                while((line=br.readLine())!=null){
+                    System.out.println("客户端说："+line);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
