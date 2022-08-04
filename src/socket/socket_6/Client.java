50f83ea33ba171f42cd3c20b633f8f7ddea09c1c
Freg <Freg@personal.com>
2022-08-04 20:16:08 +0800

    8.4 synchronized

--- src/socket/socket_6/Client.java
+++ src/socket/socket_6/Client.java
@@ -1,4 +1,45 @@
 package socket.socket_6;
 
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class Client {
+    private Socket socket;
+
+    public Client(){
+        try {
+            socket = new Socket("localhost",8088);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start(){
+        try {
+            OutputStream os = socket.getOutputStream();
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os, StandardCharsets.UTF_8));
+            PrintWriter pw = new PrintWriter(bw,true);
+
+            Scanner scanner = new Scanner(System.in);
+            while(true){
+                String line = scanner.nextLine();
+                if ("exit".equals(line)){
+                    System.out.println("已退出");
+                    socket.close();
+                    break;
+                }
+                pw.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.start();
+    }
 }
--- src/socket/socket_6/Server.java
+++ src/socket/socket_6/Server.java
@@ -1,4 +1,71 @@
 package socket.socket_6;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
 public class Server {
+    private ServerSocket serverSocket;
+
+    public Server(){
+        try {
+            serverSocket = new ServerSocket(8088);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start(){
+        try {
+            System.out.println("等待客户端连接···");
+            while(true){
+                Socket socket = serverSocket.accept();
+                System.out.println("已连接");
+                ClientHandler handler = new ClientHandler(socket);
+                Thread thread = new Thread(handler);
+                thread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class ClientHandler implements Runnable{
+        private String host;
+        private Socket socket;
+
+        public ClientHandler(Socket socket){
+            this.socket=socket;
+            host = socket.getInetAddress().getHostAddress();
+        }
+
+        public void run(){
+            try {
+                InputStream is = socket.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+                String line;
+                while(true){
+                    line= br.readLine();
+                    if (line==null){
+                        System.out.println("客户端已退出连接");
+                        socket.close();
+                        break;
+                    }
+                    System.out.println(host+"： "+line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.start();
+    }
+
 }
