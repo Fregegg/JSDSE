cd897cef62bdeae3173e6e4aa63178284f78d84e
Freg <Freg@personal.com>
2022-08-07 21:55:11 +0800

    8.7

--- src/socket/socket_9/Client.java
+++ src/socket/socket_9/Client.java
@@ -1,4 +1,36 @@
 package socket.socket_9;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
+    private Socket socket;
+
+    public Client(){
+        try {
+            socket = new Socket("localhost",8088);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void start(){
+
+    }
+
+    private class ServerHandler implements Runnable{
+        private String host;
+        private Socket socket;
+
+        public ServerHandler(){
+
+        }
+
+        public void run(){
+
+        }
+
+
+    }
+
 }
