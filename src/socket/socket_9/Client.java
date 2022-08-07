b29894050b252a2251062f27e662d48041886064
Freg <Freg@personal.com>
2022-08-07 22:16:15 +0800

    8.7

--- src/socket/socket_9/Client.java
+++ src/socket/socket_9/Client.java
@@ -1,7 +1,9 @@
 package socket.socket_9;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class Client {
     private Socket socket;
@@ -14,7 +16,26 @@ public class Client {
         }
     }
 
+    Scanner scanner = new Scanner(System.in);
     public void start(){
+        ServerHandler serverHandler = new ServerHandler(socket);
+        Thread thread = new Thread(serverHandler);
+        thread.setDaemon(true);
+        thread.start();
+
+        try {
+            PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(),StandardCharsets.UTF_8)),true);
+            String line = null;
+            while(true){
+                line=scanner.nextLine();
+                if ("exit".equals(line)){
+                    break;
+                }
+                pw.println(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
@@ -22,12 +43,21 @@ public class Client {
         private String host;
         private Socket socket;
 
-        public ServerHandler(){
-
+        public ServerHandler(Socket socket ){
+            host = socket.getInetAddress().getHostAddress();
+            this.socket=socket;
         }
 
         public void run(){
-
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+                String line ;
+                while((line=br.readLine())!=null){
+                    System.out.println(line);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
 
 
